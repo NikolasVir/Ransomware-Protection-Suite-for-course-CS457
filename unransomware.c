adbfdf4eb2ce24b35c746e9d6e041a79eb6c8d31
Nikolas VIrintzakis <nikvir@outlook.com>
2024-03-25 13:07:28 +0200

    Fixed unransomware errors

--- unransomware.c
+++ unransomware.c
@@ -2,13 +2,25 @@
 #include <stdlib.h>
 #include <string.h>
 
+void unlock_file(const char *locked_filename);
+
+int main(int argc, char *argv[]) {
+    if(argc != 2) {
+        printf("Usage: %s <locked_filename>\n", argv[0]);
+        return 1;
+    }
+    
+    unlock_file(argv[1]);
+    
+    return 0;
+}
 
 void unlock_file(const char *locked_filename) {
     FILE *locked_file, *unlocked_file;
     char ch;
-    char original_filename[1024];
+    char original_filename[100];
     char *extension_position;
-
+    
     if((extension_position = strstr(locked_filename, ".locked")) == NULL) {
         printf("Error: The specified file is not in locked format.\n");
         exit(1);
@@ -23,12 +35,9 @@ void unlock_file(const char *locked_filename) {
         exit(1);
     }
     
-    char newFilename[strlen(original_filename) + 10];
-    snprintf(newFilename, sizeof(newFilename), "%s.unlocked", original_filename);
-    
-    unlocked_file = fopen(newFilename, "w");
+    unlocked_file = fopen(original_filename, "w");
     if(unlocked_file == NULL) {
-        printf("Error: Unable to create unlocked file %s.\n", newFilename);
+        printf("Error: Unable to create unlocked file %s.\n", original_filename);
         fclose(locked_file);
         exit(1);
     }
@@ -44,18 +53,6 @@ void unlock_file(const char *locked_filename) {
         printf("Error: Unable to delete locked file %s.\n", locked_filename);
         exit(1);
     } else {
-        printf("File %s unlocked to %s and deleted successfully.\n", locked_filename, newFilename);
+        printf("File %s unlocked to %s and deleted successfully.\n", locked_filename, original_filename);
     }
-}
-
-
-int main(int argc, char *argv[]) {
-    if(argc != 2) {
-        printf("Usage: %s <locked_filename>\n", argv[0]);
-        return 1;
-    }
-    
-    unlock_file(argv[1]);
-    
-    return 0;
 }
